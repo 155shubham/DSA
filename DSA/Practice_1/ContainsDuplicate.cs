1c5b3f067c7252f47f5f099665ae0e05778abb3e
Shubham <155.shubham@gmail.com>
2026-04-11 12:58:06 +0530

    ProductofArrayExceptSelf and ContainsDuplicate

---
+++ DSA/Practice_1/ContainsDuplicate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Practice_1;
+public class ContainsDuplicate
+{
+    // O(n)
+    public static bool ContainsDuplicateMethod(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if(hashSet.Contains(num))
+            {
+                return true;
+            }
+            hashSet.Add(num);
+        }
+        return false;
+    }
+
+    // This is also O(n) but it is more cleaner
+    public static bool ContainsDuplicateMethod_MoreCleaner(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if (!hashSet.Add(num))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ DSA/Practice_1/ProductofArrayExceptSelf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Practice_1;
+public class ProductofArrayExceptSelf
+{
+    public static int[] Find_ProductofArrayExceptSelf(int[] arr)
+    {
+        var result = new int[arr.Length];
+        result[0] = 1;
+        int left = 1;
+        for (int i= 1; i < arr.Length; i++)
+        {
+            result[i] = result[i - 1] * arr[i-1];
+        }
+        int right = 1;
+        for (int j = arr.Length-1; j >=0; j--)
+        {
+            if (j == arr.Length-1)
+            {
+                result[j] *= right;
+                result[j] = result[j + 1] * arr[j + 1];
+            }
+            else
+            {
+                
+            }
+        }
+
+        return result;
+
+    }
+}
