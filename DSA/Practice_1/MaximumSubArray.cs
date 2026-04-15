ba9d4ceaa8064263fe4d20b01ef7806f4ca99181
Shubham <155.shubham@gmail.com>
2026-04-15 10:24:03 +0530

    Maximum Subarray

---
+++ DSA/Practice_1/MaximumSubArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Practice_1;
+public class MaximumSubArray
+{
+    public static int Find_MaximumSubArray(int[] arr)
+    {
+        int maxSum = int.MinValue;
+        int currentSum = 0;
+        foreach (var item in arr)
+        {
+            currentSum = Math.Max(item, currentSum + item);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
